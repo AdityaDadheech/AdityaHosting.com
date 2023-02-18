299b49bf942b4b89032eb4af48698d20585916f0
AdityaDadheech <79350931+AdityaDadheech@users.noreply.github.com>
2023-02-19 01:22:31 +0530

    Update: updated JS to play stop move forward and backward the music

--- script.js
+++ script.js
@@ -1,49 +1,137 @@
 console.log("Welcome to musico");
 //initializing the elements
-let songIndex = 0;
-let audioElement = new Audio('songs/IWL.mpeg');
-let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar');
-let gif = document.getElementById('gif');
-let songItems = Array.from(document.getElementsByClassName('songsItem'));
+let songIndex = 1; // it is used to access the songs
+let audioElement = new Audio("songs/1.mp3");//initializing the default audio
+let masterPlay = document.getElementById("masterPlay");//play button
+let myProgressBar = document.getElementById("myProgressBar");//progress bar
+let gif = document.getElementById("gif"); // equalizer
+let masterClassSong = document.getElementById("masterClassSong"); //song name on player
+let songItems = Array.from(document.getElementsByClassName("songsItem"));//array of objects in class name songsItem
 
+// initializing songs and their paths 
 let songs = [
-    {songName:"Ishq waala love", filePath:"songs/IWL.mpeg", coverPath: "covers/1.jpg"},
-    {songName:"Koi aisa geet gau", filePath:"songs/koiGeet.mp3", coverPath: "covers/2.jpg"},
-    {songName:"Hum to deewane hue", filePath:"songs/humToDewwane.mp3", coverPath: "covers/3.jpg"},
-    {songName:"Badshah-o-Badshah", filePath:"songs/Badshah.mp3", coverPath: "covers/4.jpg"},
-    {songName:"Ishq waala love", filePath:"songs/infected.mpeg", coverPath: "covers/background2.jpg"},
-]
+  {
+    songName: "Khairiyat",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "Koi aisa geet gau",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
+  },
+  {
+    songName: "Hum to deewane hue",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
+  },
+  {
+    songName: "Badshah-o-Badshah",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
+  },
+  {
+    songName: "chashni",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/background2.jpg",
+  },
+];
 
-songItems.forEach((element, i)=>{
-    element.getElementsByTagName('img')[0].src = songs[i].coverPath;
-    console.log(element, i);
-})
-//audioElement.play(); 
+//changing the cover image and song name in the songs list
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 
-masterPlay.addEventListener('click',()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
+//click event for play button
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
 
+// changing the progress bar according to the song
+audioElement.addEventListener("timeupdate", () => {
+  let progress = (audioElement.currentTime / audioElement.duration) * 100;
+  //console.log(progress);
+  myProgressBar.value = progress;
+});
+
+// changing the song according to the change occur in progress bar
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+// funtion that make all the buttons in the song list to play 
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songPlay")).forEach((element) => {
+    element.classList.add("fa-play-circle");
+    element.classList.remove("fa-pause-circle");
+  });
+};
+
+// click event for the songs to be get played when clicked on them
+//Array.from is used to iterate through the document of html which 
+//contains that particular class mention in getElementsByClassName
+Array.from(document.getElementsByClassName("songPlay")).forEach(
+  (element, i) => {
+    element.addEventListener("click", (e) => {
+      console.log(e.target);
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src = `songs/${songIndex}.mp3`;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity = 1;
+      masterClassSong.innerText = songs[songIndex-1].songName;
+      masterPlay.classList.remove("fa-play-circle");
+      masterPlay.classList.add("fa-pause-circle");
+    });
+  }
+);
+// next song to get played when clicked on next button
+document.getElementById("forward").addEventListener("click", () => {
+  if (songIndex >= 5) {
+    songIndex = 1;
+  } else {
+    songIndex += 1;
+  }
+  console.log(songIndex);
+  audioElement.src = `songs/${songIndex}.mp3`;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  gif.style.opacity = 1;
+  masterClassSong.innerText = songs[songIndex-1].songName;
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
+
+//previous song to get played when clicked on previous button 
+document.getElementById("backward").addEventListener("click", () => {
+    if (songIndex <= 1) {
+      songIndex = 5;
+    } else {
+      songIndex -= 1;
     }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
-    }
-})
+    console.log(songIndex);
+    audioElement.src = `songs/${songIndex}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterClassSong.innerText = songs[songIndex-1].songName;
+    gif.style.opacity = 1;
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
 
-audioElement.addEventListener('timeupdate',()=>{
-    let progress  = ((audioElement.currentTime/audioElement.duration)*100);
-    //console.log(progress);
-    myProgressBar.value = progress;
-})
-
-myProgressBar.addEventListener('change',()=>{
-    audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
-})
-
+  });
+  
